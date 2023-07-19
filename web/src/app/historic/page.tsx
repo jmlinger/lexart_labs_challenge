dfa8b38fe281ec5bc981ea0ac1ce81b2d89f8e62
jmlinger <jmlinger10@gmail.com>
2023-07-19 15:55:32 -0300

    feat: it adds react-csv.

--- web/src/app/historic/page.tsx
+++ web/src/app/historic/page.tsx
@@ -5,20 +5,17 @@ import { api } from "@/lib/api";
 import { ChevronLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-
-interface Ifile {
-  name: string
-  url: string
-}
+import { CSVLink } from "react-csv";
 
 export default function Historic() {
-  const [file, setFile] = useState<Ifile>()
+  const [file, setFile] = useState<any>()
   const router = useRouter()
 
   async function handleSubmit (e: any) {
     e.preventDefault()
 
     const fileResponse = await api.get('/conversation')
+    
     setFile(fileResponse.data)
   }
 
@@ -28,10 +25,9 @@ export default function Historic() {
       <ChevronLeft />
         Come back to chat
       </Button>
-      
       <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
           <Button size='lg' className='text-xl h-16' type="submit">Generate historic conversation</Button>
-          <a className={`${file ? 'block' : 'hidden'} inline-flex bg-slate-400 h-10 hover:bg-slate-300 text-xl items-center justify-center rounded-md font-medium ring-offset-background transition-colors`} href={file?.url} download={file?.name}>Download</a>
+          {file && <CSVLink data={file} className="inline-flex bg-slate-400 h-10 hover:bg-slate-300 text-xl items-center justify-center rounded-md font-medium ring-offset-background transition-colors">Download</CSVLink>}
       </form>
     </div>
   )
