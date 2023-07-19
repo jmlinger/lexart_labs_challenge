a8fb65c9e15e4f3c96698f51dc65508707a1e648
jmlinger <jmlinger10@gmail.com>
2023-07-19 12:29:58 -0300

    feat: it changes loan informations.

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -28,33 +28,31 @@ async function main() {
     data: {
       group: 'loan info',
       keywords: 'loan',
-      message:
-        'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout:',
+      message: 'Frequently asked questions about loan:',
       options: 'Do you want to apply for a loan?',
-      info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      references: 'https://www.lipsum.com/',
+      info: 'A personal loan is money that you borrow for just about any purpose. You can get a personal loan through a bank, credit union, credit card issuer, or online financial lender. You can normally apply online or in person and will have to provide some basic personal a Rinancial information. Your lender will consider your employment status, incom Rutstanding debts, and overall credit score to assess whether you are eligible for a loan a Retermine the interest rate that you will pay.',
+      references:
+        'https://www.investopedia.com/articles/personal-finance/010516/how-apply-personal-loan.asp',
     },
   })
   const info2 = await prisma.answer.create({
     data: {
       group: 'loan info',
       keywords: 'loan',
-      message:
-        'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout:',
+      message: 'Frequently asked questions about loan:',
       options: 'Loan conditions',
-      info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      references: 'https://www.lipsum.com/',
+      info: 'These Loan Conditions will apply to each Loan Contract entered into through the Lending Platform for a cash loan. We will pay the amount of credit to your Nominated Account. You must repay the total amount payable under each Loan Contract by paying the Repayments on the Repayment Date shown in the Loan Contract or, where you enter into more than one Loan Contract at or about the same time.',
+      references: 'https://secure2.zopa.com/loan_conditions',
     },
   })
   const info3 = await prisma.answer.create({
     data: {
       group: 'loan info',
       keywords: 'loan',
-      message:
-        'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout:',
+      message: 'Frequently asked questions about loan:',
       options: 'Help',
-      info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      references: 'https://www.lipsum.com/',
+      info: 'What can we help you with today? Loans, Financial support, Credit card, Car finance, Investments, Smart Savings Hub, Fixed Term Savings, Borrowing Power, Zopa app, My Zopa account, Uploading documents.',
+      references: 'https://www.zopa.com/help',
     },
   })
   const farewell = await prisma.answer.create({
